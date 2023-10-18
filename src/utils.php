e577edc46cf195feeaac0fd25256d9ddb557c26e
guoyang33 <guoyang33@gmail.com>
2023-10-18 15:31:31 +0800

    文案目錄常數、引入文案函數

--- src/utils.php
+++ src/utils.php
@@ -3,11 +3,21 @@
  * utils.php
  * 通用工具函数
  */
+define('CONTENT_JSON_DIR', 'content_json/');
 define('LANG_DICT', array(
     'zh_TW' => '繁體中文',
     'en' => 'English'
 ));
-// 召喚語言切換
+// 載入文案content json；定義CONTENT_JSON常數及返回該常數
+function load_content_json() {
+    global $page;
+    global $lang;
+    if (!defined('CONTENT_JSON')) {
+        define('CONTENT_JSON', json_decode(file_get_contents("{CONTENT_JSON_DIR}.{$page}.json"), true)[$lang]);
+    }
+    return CONTENT_JSON[$lang];
+}
+// 召喚語言切換選單
 function summon_lang_switch($lang, $page) {
     echo '<div class="dropdown mx-3">';
     echo '<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">';
@@ -19,7 +29,7 @@ function summon_lang_switch($lang, $page) {
     echo '</ul>';
     echo '</div>';
 }
-// 召喚亮色模板語言切換
+// 召喚亮色模板語言切換選單
 function summon_lang_switch_light($lang, $page) {
     echo '<div class="dropdown mx-3">';
     echo '<button class="btn btn-light border-dark dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">';
