7fb4d92c0fec6b15bac62ab19031ee4ce1b03e48
guoyang33 <guoyang33@gmail.com>
2023-12-12 08:41:41 +0800

    因應PHP5，取消跨腳本常數，改使用一般變數

--- src/html_head.php
+++ src/html_head.php
@@ -17,27 +17,28 @@ if (isset($_COOKIE['lang'])) {
 // 載入info文案
 $info = load_info_json($lang);
 $content = load_content_json($lang);
+$lang_dict = get_lang_dict();
 
 echo '
-<html lang="' . LANG_DICT[$lang]['html_lang'] . '">
-<head>
-<meta charset="utf-8">
-<meta name="Content-Type" content="text/html; charset=utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="' . $info['description'] . '" />
-<title>' . $info['title'] . '</title>
-<link rel="icon" href="' . $info['icon'] . '" type="image/x-icon" />
+<html lang="' . $lang_dict[$lang]['html_lang'] . '">
+    <head>
+        <meta charset="utf-8">
+        <meta name="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="' . $info['description'] . '" />
+        <title>' . $info['title'] . '</title>
+        <link rel="icon" href="' . $info['icon'] . '" type="image/x-icon" />
 
-<!-- 重置網頁CSS設定 -->
-<link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
-<!-- 載入Bootstrap 5 -->
-<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
-<!-- 自定義CSS設定 -->
-<link href="assets/dist/css/style.css" rel="stylesheet" cache="false" />
-<!-- jQuery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- 重置網頁CSS設定 -->
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
+        <!-- 載入Bootstrap 5 -->
+        <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- 自定義CSS設定 -->
+        <link href="assets/dist/css/style.css" rel="stylesheet" cache="false" />
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-</head>
+    </head>
 ';
 ?>
