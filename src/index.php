d30e3ca65c45de7ef12050cdfab3d4156d8c068d
guoyang33 <guoyang33@gmail.com>
2023-12-12 08:45:16 +0800

    為了便容易檢示HTML原始碼，暫時不再使用跨腳本函數來輸出HTML。網頁將以一頁式呈現

--- src/index.php
+++ src/index.php
@@ -10,8 +10,40 @@ require_once 'utils.php';
 echo '
 <body>
     <div id="home" class="big-bg">
+        <header class="page-header wrapper">
+            
+            <h1>
+                <a href="index.php">
+                    <img class="logo" src="' . $info['logo'] . '" alt="' . $info['logo_alt'] . '">
+                </a>
+            </h1>
+            <div class="header-function-bar">
+                <div class="dropdown dropdown-language-switch">
+                    語言：
+                    <button class="btn dropdown-toggle border" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="dropdown-text">Language</span>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="?lang=zh_TW">繁體中文</a></li>
+                        <li><a class="dropdown-item" href="?lang=en">English</a></li>
+                    </ul>
+                </div>
+                <nav>
+                    <ul class="main-nav">
 ';
-html_body_header($info);
+    foreach ($info['header']['nav'] as $nav_item) {
+        echo '
+                    <li>
+                        <a href="' . $nav_item['link'] . '">' . $nav_item['text'] . '</a>
+                    </li>
+        ';
+    }
+echo '
+                    </ul>
+                </nav>
+            </div>
+        </header>
+';
 echo '
         <div class="home-content wrapper">
             <h2 class="page-title">' . $content['title'] . '</h2>
