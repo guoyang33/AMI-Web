2ec8a52f871c6708af8456a288df2a4790d53ead
guoyang33 <guoyang33@gmail.com>
2024-01-14 16:27:58 +0800

    重回原案，一頁式+獨立頁

--- src/index.php
+++ src/index.php
@@ -1,36 +1,12 @@
-<?php
-/* 
- * index.php
- * 入口網頁(首頁)
- */
-
-// require_once 'html_head.php';
-require_once 'utils.php';
-
-header("Content-Type: text/html; charset=utf-8");
-$lang = 'en';
-if (isset($_GET['lang'])) {
-    setcookie('lang', $_GET['lang'], time() + 3600 * 24 * 30, '/');
-    header('Location: ' . basename($_SERVER['PHP_SELF']));
-}
-if (isset($_COOKIE['lang'])) {
-    $lang = $_COOKIE['lang'];
-}
-
-// 載入info文案
-$info = load_info_json($lang);
-$content = load_content_json($lang);
-$lang_dict = get_lang_dict();
-
-echo '
 <!DOCTYPE html>
-<html lang="' . $lang_dict[$lang]['html_lang'] . '">
+
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="' . $info['description'] . '" />
-        <title>' . $info['title'] . '</title>
+        <meta name="description" content="AWESOME Medical Inc. (abbr. as AMI), a medical in-vitro device (IVD) and medicine import/ distribution company based in Kaohsiung city, Taiwan." />
+        <title>Awesome Medical</title>
         <link rel="apple-touch-icon" sizes="57x57" href="./assets/icon/apple-icon-57x57.png">
         <link rel="apple-touch-icon" sizes="60x60" href="./assets/icon/apple-icon-60x60.png">
         <link rel="apple-touch-icon" sizes="72x72" href="./assets/icon/apple-icon-72x72.png">
@@ -56,150 +32,46 @@ echo '
         <!-- 自定義CSS設定 -->
         <link href="assets/dist/css/style.css" rel="stylesheet" cache="false" />
         <!-- jQuery -->
-        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
-';
 
-echo '
     <body>
-        <header id="header" class="header fixed-top d-none">
-            <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-                <a href="index.php" class="logo d-flex align-items-center">
-                    <img class="logo" src="' . $info['logo'] . '" alt="' . $info['logo_alt'] . '">
+        <!-- header -->
+        <header>
+            <div class="header-logo">
+                <a href="index.php">
+                    <img src="assets/awe_logo.png" alt="Awesome Medical Inc.">
+                    <!-- <p>高展醫療用品事業有限公司</p>
+                    <p>Awesome Medical Inc.</p> -->
                 </a>
-                <nav class="navbar">
-                    <ul>
-                        <li><a class="nav-link scrollto active" href="#home">Home</a></li>
-                        <li><a class="nav-link scrollto" href="#about">About</a></li>
-                        <li><a class="nav-link scrollto" href="#products">Products</a></li>
-                        <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-                        <li><a class="nav-link scrollto" href="#news">news</a></li>
-                    </ul>
-                    <i class="bi mobile-nav-toggle bi-list"></i>
-                </nav><!-- .navbar -->
-
-
-                <div class="header-function-bar d-none">
-                    <div class="dropdown dropdown-language-switch">
-                        語言：
-                        <button class="btn dropdown-toggle border" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="dropdown-text">Language</span>
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="?lang=zh_TW">繁體中文</a></li>
-                            <li><a class="dropdown-item" href="?lang=en">English</a></li>
-                        </ul>
-                    </div>
-                    <nav>
-                        <ul class="main-nav">
-';
-foreach ($info['header']['nav'] as $nav_item) {
-echo '
-                            <li>
-                                <a href="' . $nav_item['link'] . '">' . $nav_item['text'] . '</a>
-                            </li>
-';
-}
-echo '
-                        </ul>
-                    </nav>
-                </div>
             </div>
-        </header><!-- /.page-header -->
-';
-/* ===========================================About=========================================== */
-echo '
-        <section id="about">
-            <h1>' . $content['about']['title'] . '</h1>
-';
-foreach ($content['about']['articles'] as $article) {
-    echo '
-            <article>
-                <h2>' . $article['title'] . '</h2>
-    ';
-    if ($article['type'] == 'text') {
-        echo '
-                <p>' . $article['text'] . '</p>
-        ';
-    } else if ($article['type'] == 'list') {
-        foreach ($article['list'] as $list_item) {
-            echo $list_item;
-        }
-    } else if ($article['type'] == 'image') {
-        echo '
-                <img src="' . $article['src'] . '" alt="' . $article['alt'] . '">
-        ';
-    }
-    echo '
-            </article>
-    ';
-}
-echo '
-        </section><!-- /#about -->
-';
-/* ===========================================Products=========================================== */
-echo '
-        <section id="products">
-            <h1>' . $content['products']['title'] . '</h1>
-';
-foreach ($content['products']['products'] as $product) {
-    echo '
-            <article class="' . $product['style_class'] . '">
-                <h2>' . $product['title'] . '</h2>
-    ';
-    if (in_array('list', $product['type'])) {       // 條列式內容
-        foreach ($product['list'] as $list_item) {
-            echo $list_item, "\n";
-        }
-    }
-    if (in_array('pro_or_pub', $product['type'])) {     // 身份選項 僅有兩個按鈕
-        echo '
-                <div class="row">
-        ';
-        echo '
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#prod-1-pro-confirm-modal">產品</button>
-                </div>
-        ';
-    }
-    echo '
-                <div class="modal fade" id="prod-1-pro-confirm-modal" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="modal-title"產品</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div><!-- /.modal-header -->
-                            <div class="modal-body">
-                                <p>你確定要選擇產品嗎？</p>
-                            </div><!-- /.modal-body -->
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">取消</button>
-                                <button type="button" class="btn btn-primary">確定</button>
-                            </div><!-- /.modal-footer -->
-                        </div><!-- /.modal-content -->
-                    </div><!-- /.modal-dialog -->
-                </div><!-- /.modal -->
-    ';
-    echo '
-            </article>
-    ';
-}
-echo '
-        </section><!-- /#products -->
-';
+            <div class="header-nav">
+                <nav class="navbar navbar-expand-sm">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#products">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#news">News</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#links">Links</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="header-lang">
+                <a href="?lang=zh_TW">切換至中文</a>
+            </div>
+        </header>
 
-echo '
-        <div id="home" class="big-bg d-none">
-            <div class="home-content wrapper">
-                <h2 class="page-title">' . $content['title'] . '</h2>
-                <p>' . $content['text'] . '</p>
-                <a class="button" href="' . $content['button']['link'] . '">' . $content['button']['text'] . '</a>
-            </div><!-- /.home-content -->
-        </div><!-- /#home -->
         <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
         <script src="assets/dist/js/main.js"></script>
     </body>
 </html>
-';
-?>
