8b93c93cdc51ceaf1c998e45e602f4d2c0231349
guoyang33 <guoyang33@gmail.com>
2024-02-02 02:16:46 +0800

    update 2024-01-31

--- src/index.php
+++ src/index.php
@@ -1,80 +1,19 @@
-<!DOCTYPE html>
+<?php
 
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="author" content="CYouLiao">
-        <meta name="description" content="AWESOME Medical Inc. (abbr. as AMI), a medical in-vitro device (IVD) and medicine import/ distribution company based in Kaohsiung city, Taiwan.">
-        <meta name="keywords" content="AWESOME Medical Inc., AMI, medical">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Awesome Medical</title>
-        <link rel="apple-touch-icon" sizes="57x57" href="./assets/icon/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="./assets/icon/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="./assets/icon/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="./assets/icon/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="./assets/icon/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="./assets/icon/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="./assets/icon/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="./assets/icon/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="./assets/icon/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="./assets/icon/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="./assets/icon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="./assets/icon/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="./assets/icon/favicon-16x16.png">
-        <link rel="manifest" href="./assets/icon/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-        <meta name="theme-color" content="#ffffff">
+include 'html_head.php';
+$content_path = 'content-json/' . $lang . '/index.json';
+$content = json_decode(file_get_contents($content_path), true);
 
-        <!-- 重置網頁CSS設定 -->
-        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
-        <!-- 載入Bootstrap 5 -->
-        <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
-        <!-- 自定義CSS設定 -->
-        <link href="assets/dist/css/style.css" rel="stylesheet" cache="false" />
-        <!-- jQuery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    </head>
-
+echo '
     <body>
-        <header>
-            <div class="header-logo">
-                <a href="index.php">
-                    <img src="assets/awe_logo.png" alt="Awesome Medical Inc.">
-                    <!-- <p>高展醫療用品事業有限公司</p>
-                    <p>Awesome Medical Inc.</p> -->
-                </a>
-            </div>
-            <div class="header-nav">
-                <nav class="navbar navbar-expand-sm">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#about">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#products">Products</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#news">News</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#contact">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#links">Links</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="header-lang">
-                <a href="?lang=zh_TW">切換至中文</a>
-            </div>
-        </header>
+';
+html_body_header(true, $lang);
 
+echo '
         <section id="about">
             <h1>高展醫療用品</h1>
             <h2>追求卓越，超越期許，與您共創健康未來。</h2>
-            <button class="btn btn-primary" onclick="location.href='about.php'">關於高展</button>
+            <button class="btn btn-primary" onclick="location.href=\'about.php\'">關於高展</button>
         </section><!-- #about -->
         <section id="products">
         </section><!-- #products -->
@@ -97,3 +36,6 @@
         <script src="assets/dist/js/main.js"></script>
     </body>
 </html>
+';
+
+?>
