094b861decf5bfdeface13bc51918cc5d3c98a5d
guoyang33 <guoyang33@gmail.com>
2024-03-28 14:16:44 +0800

    測試password相關函數

--- src/db_test.php
+++ src/db_test.php
@@ -5,9 +5,12 @@
  */
 require_once 'connect_db.php';
 
-$sth = $dbh->prepare("SHOW TABLES");
-$sth->execute();
-$tables = $sth->fetchAll(PDO::FETCH_ASSOC);
-print_r($tables);
+echo password_hash('pass1234', PASSWORD_DEFAULT);
+// 正確使用 hash前段不能少
+var_dump(password_verify('pass1234', '$2y$10$4K2xPt82OC.3LGZKqUSDTOIE6O1NTKjP7endKr2FELDMHR4jji156'));
+// 去掉 hash前段會錯誤
+var_dump(password_verify('pass1234', '3LGZKqUSDTOIE6O1NTKjP7endKr2FELDMHR4jji156'));
+
+var_dump(PASSWORD_DEFAULT);
 
 ?>
